5567e631709995844555b1848f706508ac1096f9
Javier Animas <animaz15@gmail.com>
2023-03-28 17:29:08 -0600

    cambios gulp y sass

---
+++ gulpfile.js
@@ -0,0 +1,23 @@
+const { src, dest, watch } = require("gulp");
+const sass = require("gulp-sass")(require('sass'));
+const plumber = require("gulp-plumber");
+
+
+function css ( done ) {
+        
+    src('src/scss/**/*.scss')// Identificar el archivo de SASS
+        .pipe( plumber() )
+        .pipe( sass() ) // Compliar el archivo
+        .pipe(dest('build/css')); // Almacenarlo en DD
+    
+    done();
+}
+
+function dev( done ) {
+    watch('src/scss/**/*.scss', css);
+
+    done();
+}
+
+exports.css = css;
+exports.dev = dev;
---
+++ js/app.js
@@ -0,0 +1,55 @@
+document.addEventListener('DOMContentLoaded', function(){
+    iniciarApp();
+});
+
+function iniciarApp() {
+    navegacionFija();
+    scrollNav();
+    idioma();
+}
+
+var check = document.querySelector('.check');
+check.addEventListener('click', idioma);
+
+function idioma () {
+    let id=check.checked;
+    if (id == true ) {
+        location.href='en/index.html'
+    } else {
+        location.href='../index.html'
+    }
+
+    console.log(check.checked);
+}
+
+function scrollNav (){
+    const enlaces = document.querySelectorAll('.navegacion-principal a');
+    enlaces.forEach( enlace => {
+        enlace.addEventListener('click', function(e){
+            e.preventDefault();
+
+            const seccionScroll = e.target.attributes.href.value;
+            const seccion = document.querySelector(seccionScroll);
+            seccion.scrollIntoView({behavior: "smooth"});
+        });
+    })
+}
+
+function navegacionFija(){
+    const barra = document.querySelector('.header');
+    const quienes = document.querySelector('#nosotros');
+    const body = document.querySelector('body');
+
+    window.addEventListener('scroll', function (){
+       
+
+        if ( quienes.getBoundingClientRect().top < 0) {
+            barra.classList.add('fijo');
+            body.classList.add('body-scroll');
+        } else {
+            barra.classList.remove('fijo');
+            body.classList.remove('body-scroll');
+        }
+    })
+
+}
